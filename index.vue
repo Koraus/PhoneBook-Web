69200065396f22b680769a600b5c4e03e878d98f
Koraus <dmytro.kashchynets@gmail.com>
2023-02-13 23:50:18 +0100

    init

---
+++ index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <TheHeader class="header"> <SearchInput /> </TheHeader>
+    <SortBy />
+    <PhonesList />
+  </div>
+</template>
+
+<script>
+import PhonesList from "../components/PhonesList.vue";
+import PhoneInput from "../components/PhoneInput.vue";
+import TheHeader from "../components/TheHeader.vue";
+import SearchInput from "../components/SearchInput";
+import SortBy from "../components/SortOrder.vue";
+
+export default {
+  name: "IndexPage",
+  components: PhonesList,
+  PhoneInput,
+  TheHeader,
+  SearchInput,
+  SortBy,
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;700&display=swap");
+@import url('https://fonts.cdnfonts.com/css/clash-display');
+</style>
+
+<style lang="scss" scoped>
+.container {
+  padding: 8px;
+}
+.header{
+  margin-bottom: 20px;
+}
+</style>
